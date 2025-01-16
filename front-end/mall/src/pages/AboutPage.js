5bbe1d4bd03e899c8c5278a817e7b109a874b203
choseongin0815 <chobocho0815@gmail.com>
2025-01-16 17:58:32 +0900

    error slove

---
+++ front-end/mall/src/pages/AboutPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BasicLayout from '../layouts/BasicLayout';
+
+function AboutPage(props) {
+    return (
+        <BasicLayout>
+            AboutPage
+        </BasicLayout>
+    );
+}
+
+export default AboutPage;
---
+++ front-end/mall/src/pages/MainPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import BasicLayout from '../layouts/BasicLayout';
+function MainPage(props) {
+    return (
+       <BasicLayout>
+            <div>Main Pages</div>
+       </BasicLayout>
+    );
+}
+
+export default MainPage
